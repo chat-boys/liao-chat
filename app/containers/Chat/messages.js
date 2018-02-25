51845d451e2b0b31f769f125b615093c1f5a5bc3
Dan Park <danieljinwoopark@gmail.com>
2018-02-24 19:13:45 -0600

    chat container

---
+++ app/containers/Chat/messages.js
@@ -0,0 +1,13 @@
+/*
+ * Chat Messages
+ *
+ * This contains all the text for the Chat component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.containers.Chat.header',
+    defaultMessage: 'This is the Chat container !',
+  },
+});
