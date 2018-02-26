170b13251b8edf4fd807b12f70724c075ecd2ee1
Tom Lagier <tom@gliffy.com>
2018-02-25 17:03:12 -0800

    add reducer

--- app/containers/ChatContainer/reducer.js
+++ app/containers/ChatContainer/reducer.js
@@ -7,14 +7,22 @@
 import { fromJS } from 'immutable';
 import {
   SEND_MESSAGE_ACTION,
+  RECEIVE_MESSAGE_ACTION,
 } from './constants';
 
-const initialState = fromJS({});
+const initialState = fromJS({
+  messages: [],
+});
 
 function chatContainerReducer(state = initialState, action) {
   switch (action.type) {
     case SEND_MESSAGE_ACTION:
       return state;
+    case RECEIVE_MESSAGE_ACTION:
+      return {
+        messages: state.messages.push(action.message),
+        ...state,
+      };
     default:
       return state;
   }
