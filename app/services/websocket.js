a83ae1618c11c908b69194309a4fb94be383b12e
Tom Lagier <tom@gliffy.com>
2018-03-03 22:11:05 -0800

    downgrade to http

--- app/services/websocket.js
+++ app/services/websocket.js
@@ -1,6 +1,7 @@
+// TODO: Upgrade to WSS once supported by GAE
 const wsUrl = process.env.WS_HOST
-  ? `wss://${process.env.WS_HOST}`
-  : 'ws://localhost:3000';
+  ? `ws://${process.env.WS_HOST}/ws`
+  : 'ws://localhost:3000/ws';
 const ws = new WebSocket(wsUrl);
 
 export default ws;
