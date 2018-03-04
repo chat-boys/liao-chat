2c888c800117a6a195243eb7ba7dfd04640c1563
Tom Lagier <tom@gliffy.com>
2018-03-03 22:43:16 -0800

    try

--- app/services/websocket.js
+++ app/services/websocket.js
@@ -1,6 +1,5 @@
-// TODO: Upgrade to WSS once supported by GAE
 const wsUrl = process.env.WS_HOST
-  ? `ws://${process.env.WS_HOST}/ws`
+  ? `wss://${process.env.WS_HOST}/ws`
   : 'ws://localhost:3000/ws';
 const ws = new WebSocket(wsUrl);
 
